8fd9130fe1b1c687cc3d3a5aed8c7e9d91c040c7
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2017-09-19 20:33:25 -0500

    hardcoded ram instruction intead of reading .list file to synthetise

--- genram.v
+++ genram.v
@@ -10,7 +10,23 @@ module genram #(             //-- Parametros
          output reg [DW-1: 0] data_out); //-- Dato a escribir
 
 //-- Parametro: Nombre del fichero con el contenido de la RAM
-parameter ROMFILE = "bufferini.list";
+  
+initial ram[0] = 32'h00022000;
+initial ram[1] = 32'h0BBBBBBB;
+initial ram[2] = 32'h00aa0000;
+initial ram[3] = 32'h0AAAAAAA;
+initial ram[4] = 32'h00aaa000;
+initial ram[5] = 32'h00aaa000;
+initial ram[6] = 32'h00000001;
+initial ram[7] = 32'h0eee0000;
+initial ram[8] = 32'h00f00000;
+initial ram[9] = 32'h00f00000;
+initial ram[10] = 32'h0aaa0001;
+initial ram[11] = 32'h0f000000;
+initial ram[12] = 32'h0f000000;
+initial ram[13] = 32'h0f000000;
+initial ram[14] = 32'h0f000000;
+initial ram[15] = 32'h0ff00000;
 
 //-- Calcular el numero de posiciones totales de memoria
 localparam NPOS = 2 ** AW;
@@ -24,10 +40,6 @@ localparam NPOS = 2 ** AW;
     data_out <= ram[addr];
   end
 
-  //-- Escritura en la memoria
-  
-  
-  
   always @ * begin
     if (rw == 0)
      ram[addr] <= data_in;
@@ -35,8 +47,6 @@ localparam NPOS = 2 ** AW;
 
 //-- Cargar en la memoria el fichero ROMFILE
 //-- Los valores deben estan dados en hexadecimal
-initial begin
-  $readmemh(ROMFILE, ram);
-end
+
 
 endmodule
